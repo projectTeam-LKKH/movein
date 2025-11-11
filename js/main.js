74624f893be22fef167eed4be8f9906ea7f70208
WELCOMEtohaworld <habinkim0427@naver.com>
2025-11-11 14:58:44 +0900

    index. html수정 : favorite-gutter클래스 디브 추가, genre-bubbles연결

--- js/main.js
+++ js/main.js
@@ -84,66 +84,17 @@ document.addEventListener("DOMContentLoaded", () => {
 //
 const reviewBtn = document.querySelector(".review-input");
 const reviewForm = document.querySelector(".review-form");
+const prevBtn = document.querySelector(".prev-btn");
 
-reviewForm.style.display = "none";
+reviewForm.classList.remove("open");
 
 reviewBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  reviewForm.style.display =
-    reviewForm.style.display === "none" ? "block" : "none";
+  reviewForm.classList.add("open");
+
+  prevBtn.addEventListener("click", () => {
+    reviewForm.classList.remove("open");
+  });
 });
-
-//------------------아직 작업 전입니다.
-// 화면 최상단에서 아래로 스와이프하는 경우 감지
-// 기본 새로고침 동작 차단 start
-// let touchStartY = 0;
-
-// window.addEventListener(
-//   "touchstart",
-//   (e) => {
-//     touchStartY = e.touches[0].clientY;
-//   },
-//   { passive: false }
-// );
-
-// window.addEventListener(
-//   "touchmove",
-//   (e) => {
-//     const currentY = e.touches[0].clientY;
-
-//     if (window.scrollY === 0 && currentY > touchStartY) {
-//       e.preventDefault();
-//     }
-//   },
-//   { passive: false }
-// );
-
-// 화면 최상단에서 아래로 스와이프하는 경우 감지
-// 기본 새로고침 동작 차단 end
-
-// 하트 버튼 토클 start
-// document.addEventListener("DOMContentLoaded", () => {
-//   const hearts = document.querySelectorAll(".heart-icon");
-
-//   hearts.forEach((heart) => {
-//     heart.addEventListener("click", () => {
-//       heart.classList.toggle("active");
-//     });
-//   });
-// });
-// 하트 버튼 토클 end
-
-// 하단 메뉴바 start
-// document.addEventListener("DOMContentLoaded", () => {
-//   const buttons = document.querySelectorAll(".menu-btn");
-
-//   buttons.forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//       buttons.forEach((b) => b.classList.remove("active"));
-//       btn.classList.add("active");
-//     });
-//   });
-// });
-// 하단 메뉴바 end
 
 console.log("페이지가 로드되었습니다.");
